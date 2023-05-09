93bfc69071332d6941882b2e24617fb8c7fa7e56
skshrii66 <skshrii66@gmail.com>
2023-05-09 17:34:22 +0530

    'first_commit'

---
+++ dom.js
@@ -0,0 +1,36 @@
+let storedHTML= document.getElementById("para");
+
+
+let htmlData= storedHTML.innerHTML;
+let textData= storedHTML.innerText;
+console.log(htmlData);
+console.log(textData);
+
+document.getElementById("name").innerHTML= '<span>shri</span>';
+document.getElementById("age").innerHTML= '<span>22</span>';
+document.getElementById("email").innerHTML= '<span>@intgmail.com</span>';
+
+
+//apply style
+document.body.style.fontFamily = 'AriL';
+storedHTML.style.color = 'brown'
+
+//create element and append
+//element1
+let imageElement = document.createElement('img');
+imageElement.src = "download.jpg";
+imageElement.height = 300;
+imageElement.width = 300;
+
+//element2
+let diveElement = document.createElement('div');
+diveElement.innerText = "KTM"
+
+//Append created elements
+//document.body.append(imageElement,diveElement);
+//select ElementsByClassName and TagName
+let ptags = document.getElementsByTagName('p');
+let paraClass = document.getElementsByClassName('para');
+
+ptags[1].innerText = "Hello guys";
+    
