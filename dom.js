54b35de022419ae576b298bb436457579550f055
skshrii66 <skshrii66@gmail.com>
2023-05-10 18:59:19 +0530

    dom

--- dom.js
+++ dom.js
@@ -37,6 +37,40 @@ ptags[1].innerText = "Hello guys";
 function addrow(){
     let TableElement = document.getElementById('table');
     let row = TableElement.insertRow();
-    let td1 = 
+    let td1 = row.insertCell();
+    td1.innerText="CCC";
+} 
+function evenTiggered(evtValue) {
+    console.log(`Triggered event is ${evtValue.value}`)
+    //console.log(evtValue.value)
+}  
+
+function eventButton(){
+    console.log(`Button is clicked`)
 }
-    
+
+function addCountry(){
+//first of all select the select box (target a elelment to add)
+    let selectElement = document.getElementById('country');
+
+//and another create a option element
+  let newOption = document.createElement('option');
+  newOption.innerText = document.getElementById('country-value').value;
+  newOption.setAttribute('value',document.getElementById('country.value').value )
+
+  selectElement.append(newOption);
+  document.getElementById('country-value').value="";
+}
+  
+function removeCountry(){
+    let selectElement = document.getElementById('country');
+    selectElement.remove(selectElement.seletedIndex);
+}
+
+document.getElementsByTagName('ul')[0].setAttribute('id','unordered-list');
+
+let ul = document.getElementById('unordered-list');
+let li = document.createElement('li');
+li.innerHTML ='contact us';
+ul.append(li);
+
